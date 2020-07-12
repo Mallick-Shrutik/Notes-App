68a23f48b05720d113fb3efa990a2baf3a5c4a26
MallickShrutik <mallickshrutik@gmail.com>
2020-07-13 01:19:51 +0530

    Version 1

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
+import { NoteListComponent } from './pages/note-list/note-list.component';
+import { NoteCardComponent } from './note-card/note-card.component';
+import { NoteDetailsComponent } from './pages/note-details/note-details.component';
+import { FormsModule } from "@angular/forms";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainLayoutComponent,
+    NoteListComponent,
+    NoteCardComponent,
+    NoteDetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/pages/note-list/note-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Note } from 'src/app/shared/note.module';
+import { NotesService } from 'src/app/shared/notes.service';
+
+@Component({
+  selector: 'app-note-list',
+  templateUrl: './note-list.component.html',
+  styleUrls: ['./note-list.component.scss']
+})
+export class NoteListComponent implements OnInit {
+
+  notes: Note[] = new Array<Note>();
+
+  constructor(private notesService:NotesService) { }
+
+  ngOnInit(): void {
+   this.notes =  this.notesService.getAll();
+
+  }
+
+  deleteNote(id:number){
+    this.notesService.delete(id);
+
+  }
+
+}
